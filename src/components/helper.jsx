38657970f94cbbc22dae6c0aeeb4715ea80f3789
ajayprakash <ajayprakash.v@gmail.com>
2023-07-08 15:27:03 +0530

    updated

--- src/components/helper.jsx
+++ src/components/helper.jsx
@@ -2,16 +2,16 @@
 
 
 const helper = [
-    {brand:'Nike',minsize:8,maxsize:12,lowprice:49,highprice:249,image:`images/nike.png`,shoes:[{name:'Nike-1',price:59,image:'/Shoes/images/n1.png'},{name:'Nike-2',price:59,image:'/Shoes/images/n2.png'},{name:'Nike-3',price:59,image:'/Shoes/images/n3.png'},{name:'Nike-4',price:59,image:'/Shoes/images/n4.png'}]},
-    {brand:'Adidas',minsize:9,maxsize:12,lowprice:29,highprice:229,image:'images/adidas.png',
+    {brand:'Nike',minsize:8,maxsize:12,lowprice:49,highprice:249,image:`/Shoes/images/nike.png`,shoes:[{name:'Nike-1',price:59,image:'/Shoes/images/n1.png'},{name:'Nike-2',price:59,image:'/Shoes/images/n2.png'},{name:'Nike-3',price:59,image:'/Shoes/images/n3.png'},{name:'Nike-4',price:59,image:'/Shoes/images/n4.png'}]},
+    {brand:'Adidas',minsize:9,maxsize:12,lowprice:29,highprice:229,image:'/Shoes/images/adidas.png',
     shoes:[{name:'adidass-1',price:59,image:'/Shoes/images/a1.png'},{name:'adidaas-2',price:59,image:'/Shoes/images/a2.png'},{name:'adidaas-3',price:99,image:'/Shoesimages/a3.png'},{name:'adidaas-4',price:259,image:'/Shoes/images/a4.png'}]},
-    {brand:'NB',minsize:8,maxsize:12,lowprice:89,highprice:549,image:'images/nb.png',
+    {brand:'NB',minsize:8,maxsize:12,lowprice:89,highprice:549,image:'/Shoes/images/nb.png',
     shoes:[{name:'New balance-1',price:39,image:'/Shoes/images/nb1.png'},{name:'New balance-2',price:159,image:'/Shoes/images/nb2.png'},{name:'New balance-3',price:69,image:'/Shoes/images/nb3.png'},{name:'New balance-4',price:79,image:'/Shoes/images/nb4.png'}]},
-    {brand:'LOUIS VUITON',minsize:8,maxsize:12,lowprice:349,highprice:5249,image:'images/lv.png',
+    {brand:'LOUIS VUITON',minsize:8,maxsize:12,lowprice:349,highprice:5249,image:'/Shoes/images/lv.png',
     shoes:[{name:'Reebook-1',price:59,image:'/Shoes/images/r1.png'},{name:'Reebook-2',price:59,image:'/Shoes/images/r2.png'},{name:'Reebook-4',price:59,image:'/Shoes/images/r3.png'},{name:'Reebook-4',price:39,image:'/Shoes/images/r4.png'}]},
-    {brand:'GUCCI',minsize:8,maxsize:12,lowprice:549,highprice:5249,image:'images/gucci.png',
+    {brand:'GUCCI',minsize:8,maxsize:12,lowprice:549,highprice:5249,image:'/Shoes/images/gucci.png',
     shoes:[{name:'Gucci-1',price:559,image:'/Shoes/images/g1.png'},{name:'Gucci-2',price:159,image:'/Shoes/images/g2.png'},{name:'Gucci-3',price:59,image:'/Shoes/images/g3.png'},{name:'Gucci-4',price:59,image:'/Shoes/images/g4.png'}]},
-    {brand:'PUMA',minsize:8,maxsize:12,lowprice:49,highprice:249,image:'images/puma.png',
+    {brand:'PUMA',minsize:8,maxsize:12,lowprice:49,highprice:249,image:'/Shoes/images/puma.png',
     shoes:[{name:'Puma-1',price:59,image:'/Shoes/images/p1.png'},{name:'Puma-2',price:79,image:'/Shoes/images/p2.png'},{name:'Puma3',price:89,image:'/Shoes/images/p3.png'},{name:'Puma-4',price:99,image:'/Shoes/images/p4.png'}]},
 ]
 
